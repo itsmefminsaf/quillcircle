a086d1d363a61ba305de4868033e78e5f3532061
itsmefminsaf <itsmefminsaf@gmail.com>
2025-06-20 10:04:16 +0530

    feat: add navigation component and integrate it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/ui/tailwind.css";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Nav from "@/ui/components/Nav";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,7 +15,10 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 };
